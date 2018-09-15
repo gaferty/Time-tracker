f76db5a2ec079e5693799879c626e5f650e484be
gaferty <lawrenceharlow@gmail.com>
2018-09-15 21:07:25 +0100

    Main file for executing the code

---
+++ main.py
@@ -0,0 +1,8 @@
+# Three part to the project:
+# time tracker, Including: multithreading timer that will trigger an alarm function
+# A UI that will have a drop down and will be triggered by the alarm going off.
+# Fuctions that will read and write to a data base
+
+# first task build an alarm.
+# second task: make it run on a seperate thread.
+# third task, build a drop down menu.
